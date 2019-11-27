f7d164854d680e4dac268684918936b7c8dc91be
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-11-27 05:18:15 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,7 @@
 # Provided, don't edit
 require 'directors_database'
+require 'pp'
+require 'pry'
 
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
@@ -33,7 +35,16 @@ end
 
 # Your code after this point
 
-def movies_with_director_key(name, movies_collection)
+def movies_with_director_key(name, movies_collection) #iterate over movies collection. 
+  new_array = []
+  row_index = 0 
+  while row_index < movies_collection.length do 
+  new_array << movie_with_director_name(name, movies_collection[row_index]) 
+  row_index += 1 
+    
+    
+   
+  
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
   # and accumulate the returned Array of movies into a new Array that's
@@ -49,9 +60,23 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
 end
+return new_array
+end
 
 
 def gross_per_studio(collection)
+  new_hash = {}
+  column_index = 0 
+  while column_index < collection.length do 
+movies_info = collection[column_index]
+   # binding.pry
+  new_hash[movies_info[:studio]] = movies_info[:worldwide_gross]
+  
+
+  
+  column_index += 1 
+  
+    
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
@@ -64,8 +89,11 @@ def gross_per_studio(collection)
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
 end
+return new_hash
+end
 
 def movies_with_directors_set(source)
+  new_array = []
   # GOAL: For each director, find their :movies Array and stick it in a new Array
   #
   # INPUT:
